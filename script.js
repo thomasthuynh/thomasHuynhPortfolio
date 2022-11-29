e6a949f793d07d29bd3218e6b09dd21a7ae9199b
ThomasHuynh <Thomas_TTH@live.com>
2022-11-28 23:18:08 -0500

    implemented hamburger menu, added media query for 640px screen width

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburgerMenu");
+const nav = document.querySelector(".navMenu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    nav.classList.toggle("active");
+});
+
+document.querySelectorAll(".navLink").forEach(link => 
+link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    nav.classList.remove("active");
+}));
