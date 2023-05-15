ad6bdfc46cddccea899e255cbe8f23c241b3e386
ThomasHuynh <Thomas_TTH@live.com>
2023-05-15 15:44:03 -0400

    added Netflix Mini project, removed header shapes for smaller screen sizes

--- background.js
+++ background.js
@@ -1,13 +1,16 @@
 const scale = 1 / 15;
 
-const moveBackground = (event) => {
+const header = document.querySelector('header');
+
+header.addEventListener('mousemove', (event) => {
     const shapes = document.querySelectorAll('.shape');
     const x = event.clientX * scale;
     const y = event.clientY * scale;
+    console.log("This works")
 
     shapes.forEach((shape, index) => {
         const isOdd = index % 2 !== 0;
         const directionValue = isOdd ? -1 : 1;
         shape.style.transform = `translate(${x * directionValue}px, ${y * directionValue}px) rotate(${x * directionValue * 5}deg)`;
     })
-}
+})
